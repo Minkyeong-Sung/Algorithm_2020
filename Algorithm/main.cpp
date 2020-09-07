16b2974f5f0c429ca4b8ce00597958c4f111b77a
minkyeong <zoz7@naver.com>
2020-09-07 22:15:55 +0900

    N개의 최소 공배수

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,70 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <math.h>
-#include <string.h>
 
 
 /*
- 프로그래머스 : 소수 만들기
+ N개의 최소공배수 구하기
+ 
  */
 
-
-int ans = 0;
-bool prime[5000] ;
-
-
-// Check is Prime.
-void isPrime(int result){
- 
-    if(prime[result] == false){
-        ans += 1;
+// calculate GCD(최대공약수)
+int gcd(int num1, int num2){
+    
+    if(num2 ==0){
+        return num1;
+    }
+    else{
+        return gcd(num2, num1 % num2);
     }
 }
 
-// Calculate nC3 result
-void combination(int nums[], int index, int r, int result, size_t n){
+
+// arr_len은 배열 arr의 길이입니다.
+int solution(int arr[], size_t arr_len) {
+
+    int lcm = (arr[0] * arr[1] ) / gcd(arr[0], arr[1]);
     
-    if(r == 0){
-        printf("result : %d\n", result);
-        isPrime(result);
-        return;
+    for(int i=2; i<arr_len; i++){
+        lcm = lcm * arr[i] / gcd(lcm, arr[i]);
     }
-    
-    if(index > n) return;
-    
-    combination(nums, index + 1, r-1, result + nums[index], n);
-    combination(nums, index +1, r, result, n);
+    return lcm;
 }
 
 
-// nums_len은 배열 nums의 길이입니다.
-int solution(int nums[], size_t nums_len) {
-    
-    memset(prime, false, sizeof(prime));
-    
-    for(int i=2; i< 5000; i++){
-        if(prime[i]== false){
-            for(int j= i*2; j< 5000; j+= i){
-                prime[j] = true;
-            }
-        }
-    }
-    
-    // combination
-    combination(nums, 0, 3, 0, nums_len-1);
-
-    return ans;
-}
-
-
-// temp main source
+// 테스트용 
 int main(){
     
+    int arr[3]  ={2,3,4};
+    printf("%d", solution(arr, 3));
     
-    int nums[5] = {1,2,7,6,4};
     
-    solution(nums, 4);
-    
-    printf("%d", ans);
 }
