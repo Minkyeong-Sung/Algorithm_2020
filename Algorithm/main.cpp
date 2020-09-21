4760221dda9d3ba68e6dc7ba8af93e7b6593b6de
minkyeong <zoz7@naver.com>
2020-09-21 12:34:26 +0900

    1240. [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,42 +1,92 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include<iostream>
+#include <cstdio>
+#include <cstring>
 
+using namespace std;
 
-/*
- N개의 최소공배수 구하기
- 
- */
-
-// calculate GCD(최대공약수)
-int gcd(int num1, int num2){
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T, N, M;
     
-    if(num2 ==0){
-        return num1;
+    string numberType[10] = {"0001101", "0011001", "0010011", "0111101",
+        "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"};
+    
+    cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        scanf("%d %d", &N,  &M);
+        int arr[N][M] ;
+        string answer = "";
+        int flag= 0;
+        // input
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                scanf("%1d", &arr[i][j]);
+            }
+        }
+        for(int i = 0; i<N; i++){
+            for(int j=M-1; j>0; j--){
+                
+                if(arr[i][j] != 1) continue;
+                
+                // 1일 때, 7개 자리수에 대한 유형을 파악한다. ( 뒤에서 부터)
+                
+                // 여기서 56개를 7개씩 끊어서 검사해야한다.
+                for(int num = j; num > j-56; num-= 7){
+            
+                    string type ="";
+                    for(int k=num; k > num-7; k--){
+                        type += to_string(arr[i][k]);
+                    }
+                    reverse(type.begin(),  type.end());
+                
+                    for(int l = 0; l<10; l++){
+                        if(numberType[l] == type){
+                            answer += to_string(l);
+                            break;
+                        }
+                    }
+                }
+                reverse(answer.begin(), answer.end());
+                flag = 1;
+                break;
+            }
+            
+            if(flag == 1){
+                break;
+            }
+        }
+
+        if(flag == 1){
+            int num1=0, num2= 0;
+            int tmpResult = 0;
+
+            for(int i=0; i<answer.size(); i++){
+                
+                if(i%2 ==0){
+                    num1+=  (answer[i]-'0');
+                }
+                else{
+                    num2+= (answer[i]-'0');
+                }
+            }
+            tmpResult = num1 + num2;
+            num1 = num1 * 3;
+            num1 += num2;
+            
+            int tmp = num1;
+            
+            // 잘못된 코드
+            if(tmp % 10 !=0){
+                cout <<"#" << test_case << " 0\n";
+            }
+            else{
+                cout <<"#" << test_case << " " <<  tmpResult <<"\n";
+            }
+        }
     }
-    else{
-        return gcd(num2, num1 % num2);
-    }
-}
-
-
-// arr_len은 배열 arr의 길이입니다.
-int solution(int arr[], size_t arr_len) {
-
-    int lcm = (arr[0] * arr[1] ) / gcd(arr[0], arr[1]);
-    
-    for(int i=2; i<arr_len; i++){
-        lcm = lcm * arr[i] / gcd(lcm, arr[i]);
-    }
-    return lcm;
-}
-
-
-// 테스트용 
-int main(){
-    
-    int arr[3]  ={2,3,4};
-    printf("%d", solution(arr, 3));
-    
-    
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
