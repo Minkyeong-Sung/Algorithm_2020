b2adeacca312ee7a462cf5ef203ec7a8276afc8f
minkyeong <zoz7@naver.com>
2020-09-23 18:06:19 +0900

    1245. [S/W 문제해결 응용] 2일차 - 균형점

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,92 +1,69 @@
+#include <iostream>
 #include <string>
-#include <algorithm>
-#include<iostream>
 #include <cstdio>
-#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
+
+// F = G*m1*m2/(d*d), G는 양의 상수 값
+int main(){
     int test_case;
-    int T, N, M;
-    
-    string numberType[10] = {"0001101", "0011001", "0010011", "0111101",
-        "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"};
-    
+    int T, N;
+    double result = 0;
     cin>>T;
     for(test_case = 1; test_case <= T; ++test_case)
     {
-        scanf("%d %d", &N,  &M);
-        int arr[N][M] ;
-        string answer = "";
-        int flag= 0;
-        // input
+        cin >> N;
+        double x[N], m[N];
+
+        // input x-position
         for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                scanf("%1d", &arr[i][j]);
-            }
+            cin >> x[i];
         }
-        for(int i = 0; i<N; i++){
-            for(int j=M-1; j>0; j--){
-                
-                if(arr[i][j] != 1) continue;
-                
-                // 1일 때, 7개 자리수에 대한 유형을 파악한다. ( 뒤에서 부터)
-                
-                // 여기서 56개를 7개씩 끊어서 검사해야한다.
-                for(int num = j; num > j-56; num-= 7){
+        // input x-weight
+        for(int i=0; i<N; i++){
+            cin >> m[i];
+        }
+        // 이분 탐색
+        cout.precision(10);
+        cout <<"#" << test_case ;
+
+        double left, right;
+        for(int i=0; i< N-1; i++){
             
-                    string type ="";
-                    for(int k=num; k > num-7; k--){
-                        type += to_string(arr[i][k]);
-                    }
-                    reverse(type.begin(),  type.end());
+            left = x[i];
+            right = x[i+1];
+            double result = 0;
+            
+            for(int j= 0; j<50; j++){
+                double sum = 0;
+                double mid = (left + right)/2.0;
                 
-                    for(int l = 0; l<10; l++){
-                        if(numberType[l] == type){
-                            answer += to_string(l);
-                            break;
-                        }
-                    }
+                // F = G*m1*m2/(d*d), G는 양의 상수 값
+                // left
+                for(int k=0; k<= i; k++){
+                    sum += (m[k] / ((mid - x[k])*(mid - x[k])));
                 }
-                reverse(answer.begin(), answer.end());
-                flag = 1;
-                break;
-            }
-            
-            if(flag == 1){
-                break;
-            }
-        }
-
-        if(flag == 1){
-            int num1=0, num2= 0;
-            int tmpResult = 0;
-
-            for(int i=0; i<answer.size(); i++){
+                for(int k = i+1; k<N; k++){
+                    sum -= ( m[k] / ((mid - x[k])* (mid - x[k])));
+                }
                 
-                if(i%2 ==0){
-                    num1+=  (answer[i]-'0');
+                if(sum > 0){
+                    // 오른쪽 값이 더 크기때문에, 왼쪽기준을 오른쪽으로 옮겨준다.
+                    left = mid;
                 }
                 else{
-                    num2+= (answer[i]-'0');
+                    // 0 이 되면 균형점 ,
+                    right = mid;
+                    result = mid;
                 }
             }
-            tmpResult = num1 + num2;
-            num1 = num1 * 3;
-            num1 += num2;
-            
-            int tmp = num1;
-            
-            // 잘못된 코드
-            if(tmp % 10 !=0){
-                cout <<"#" << test_case << " 0\n";
-            }
-            else{
-                cout <<"#" << test_case << " " <<  tmpResult <<"\n";
-            }
+            // 소수점 아래 10자리까지 출력.
+            printf(" %.10lf", result);
         }
+        cout <<'\n';
+            
     }
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+    return 0;
 }
