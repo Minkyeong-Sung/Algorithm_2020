16662729b89050beadb33e54d78eebfac6355234
minkyeong <zoz7@naver.com>
2020-09-29 23:04:58 +0900

    1748 수 이어쓰기1

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
+#include <cmath>
 
 using namespace std;
 
 int main(){
     
-    int t, n, m, x, y;
-    cin >> t;
+    // input
+    int N;
+    cin >> N;
     
-    for(int i=0; i<t; i++){
-        
-        cin >> n >> m >> x >> y;
-        
-        bool flag = true;
-        
-        // 하나의 수를 고정하면, 최대 탐색 경우의 수는 절반이 된다.
-        for(int j=x; j< m*n; j += n){
-            
-            // 고정 시킨 후, y값을 도출해내는 조건 검색
-            if( (((j-1)%m) + 1 )== y ){
-                cout << j <<'\n';
-                flag = false;
-                break;
-            }
-        }
-        
-        // 최대로 탐색해도 답이 없을 경우 
-        if(flag){
-            cout << "-1\n";
-        }
+    int tmp = N;
+    int len = 0;
+    
+    // 총 자릿수 구하기 ( 문자 길이로 확인하는 것보다 시간이 빠르다고 한다)
+    while(tmp > 0){
+        len++;
+        tmp /= 10;
     }
+    
+    int answer = 0;
+    int i;
+    
+    // 자릿수 계산
+    for(i=1; i< len; i++){
+        answer += ( i* (pow(10, i) - pow(10, i-1)));
+    }
+    
+    answer += ((N - pow(10, i-1) + 1) * i);
+    
+    cout << answer;
+    
+    
     return 0;
 }
-
-
-/*
- || tmpY <= 4000
- **/
