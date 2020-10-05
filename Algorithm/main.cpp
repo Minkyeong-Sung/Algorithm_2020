0e7acaff16b4d7095f11fa9fae60d6e02a1158ea
minkyeong <zoz7@naver.com>
2020-10-05 21:54:17 +0900

    14889 스타트와 링크

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,77 +1,89 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-int alpha[26] ;
-int calculation(vector<string> &vt, vector<char> &letter, vector<int> &mapping){
+int arr[20][20];
+
+int calculate(vector<int> t1, vector<int> t2){
     
-    int m = letter.size();
     int sum = 0;
+    int num1 = 0, num2 = 0;
     
-    for(int i=0; i< m; i++ ){
-        alpha[ letter[i] - 65]  = mapping[i];
-    }
-    
-    for(string s : vt){
-        
-        int now=0;
-        for(char x : s){
-            now = now * 10 +  alpha[x-65] ;
+    // 대각선 라인을 제외 하고, 계산한다.
+    for(int i=0; i<t1.size(); i++){
+        for(int j=0; j<t1.size(); j++){
+         
+            if(i==j) continue;
+            num1 += arr[t1[i]][t1[j]];
         }
-        
-        sum += now;
     }
     
+    for(int i=0; i<t2.size(); i++){
+        for(int j=0; j<t2.size(); j++){
+         
+            if(i==j) continue;
+            num2 += arr[t2[i]][t2[j]];
+        }
+    }
+    
+    sum = abs(num1 - num2);
     return sum;
 }
 
 int main(){
     
     int n;
+    int ans = 987654321;
+    
+    
+    // input
     cin >> n;
     
-    vector<string> vt(n);
-    vector<char> letter;
-    
     for(int i=0; i<n; i++){
-        cin >> vt[i];
-        
-        for(char x : vt[i]){
-            letter.push_back(x);
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
         }
     }
     
-    sort(letter.begin(), letter.end());
-    letter.erase( unique(letter.begin(), letter.end()), letter.end());
+    vector<int> vt;
     
-    int m = letter.size();
-    vector<int> mapping;
-    
-    // 몇개의 숫자까지 돌아볼지 확인할 것
-    for(int i=9; i> 9-m; i--){
-        mapping.push_back(i);
+    // 팀 편성을 위해, 0과 1을 각각 넣어서 순열을 돌리기 위한 사전 작업을 한다
+    // 그래서 0인 값, 1인 값의 인덱스로 각각 나누어 본다
+    for(int i=0; i<n/2; i++){
+        vt.push_back(0);
+    }
+    for(int i=0; i<n/2; i++){
+        vt.push_back(1);
     }
     
-    // 다음 순열 사용을 위해 정렬 한다.
-    sort(mapping.begin(), mapping.end());
     
-    int answer = 0;
-    
+    // 순열 시작
     do {
-        //
-        int tmp = calculation(vt, letter, mapping);
+        vector<int> t1, t2;
         
-        if(answer < tmp){
-            answer = tmp;
+        // 순열 값을 통해서 실제 값을 저장할 때 필요한 인덱스를 저장한다.
+        for(int i=0; i<vt.size(); i++){
+            
+            if(vt[i] == 1){
+                t1.push_back(i);
+            }
+            else{
+                t2.push_back(i);
+            }
         }
         
-    } while ( next_permutation(mapping.begin(), mapping.end()));
+        // 배열에 따른 값 계산
+        ans = min ( ans, calculate(t1, t2));
+        
+    } while ( next_permutation(vt.begin(), vt.end()));
     
-    cout << answer;
     
+    cout << ans ;
     
     return 0;
 }
+
+
