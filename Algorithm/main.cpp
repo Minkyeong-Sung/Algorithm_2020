7070a18047e8fd531cb142c68d2266a3b1f59935
minkyeong <zoz7@naver.com>
2020-10-10 09:32:28 +0900

    [코딜리티] Task 6. MaxProductOfThree

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,89 +1,71 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <cstring>
 #include <cmath>
 
 using namespace std;
 
-int arr[20][20];
+/*
+ [코딜리티] Task 6. MaxProductOfThree
+ 
+ A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
 
-int calculate(vector<int> t1, vector<int> t2){
+ For example, array A such that:
+
+   A[0] = -3
+   A[1] = 1
+   A[2] = 2
+   A[3] = -2
+   A[4] = 5
+   A[5] = 6
+ contains the following example triplets:
+
+ (0, 1, 2), product is −3 * 1 * 2 = −6
+ (1, 2, 4), product is 1 * 2 * 5 = 10
+ (2, 4, 5), product is 2 * 5 * 6 = 60
+ Your goal is to find the maximal product of any triplet.
+
+ Write a function:
+
+ int solution(vector<int> &A);
+
+ that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+ For example, given array A such that:
+
+   A[0] = -3
+   A[1] = 1
+   A[2] = 2
+   A[3] = -2
+   A[4] = 5
+   A[5] = 6
+ the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+ Write an efficient algorithm for the following assumptions:
+
+ N is an integer within the range [3..100,000];
+ each element of array A is an integer within the range [−1,000..1,000].
+ 
+ 
+ */
+
+int solution(vector<int> &A) {
+    int ans = 0;
     
-    int sum = 0;
-    int num1 = 0, num2 = 0;
+    sort(A.begin(), A.end());
+    int size = (int)A.size();
     
-    // 대각선 라인을 제외 하고, 계산한다.
-    for(int i=0; i<t1.size(); i++){
-        for(int j=0; j<t1.size(); j++){
-         
-            if(i==j) continue;
-            num1 += arr[t1[i]][t1[j]];
-        }
+    int result1 = A[0] * A[1] * A[size-1];
+    int result2 = A[size-1] * A[size-2] * A[size-3];
+    
+    if(result1 > result2 ){
+        ans = result1;
+    }
+    else{
+        ans = result2;
     }
     
-    for(int i=0; i<t2.size(); i++){
-        for(int j=0; j<t2.size(); j++){
-         
-            if(i==j) continue;
-            num2 += arr[t2[i]][t2[j]];
-        }
-    }
-    
-    sum = abs(num1 - num2);
-    return sum;
+    return ans;
 }
-
-int main(){
-    
-    int n;
-    int ans = 987654321;
-    
-    
-    // input
-    cin >> n;
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> arr[i][j];
-        }
-    }
-    
-    vector<int> vt;
-    
-    // 팀 편성을 위해, 0과 1을 각각 넣어서 순열을 돌리기 위한 사전 작업을 한다
-    // 그래서 0인 값, 1인 값의 인덱스로 각각 나누어 본다
-    for(int i=0; i<n/2; i++){
-        vt.push_back(0);
-    }
-    for(int i=0; i<n/2; i++){
-        vt.push_back(1);
-    }
-    
-    
-    // 순열 시작
-    do {
-        vector<int> t1, t2;
-        
-        // 순열 값을 통해서 실제 값을 저장할 때 필요한 인덱스를 저장한다.
-        for(int i=0; i<vt.size(); i++){
-            
-            if(vt[i] == 1){
-                t1.push_back(i);
-            }
-            else{
-                t2.push_back(i);
-            }
-        }
-        
-        // 배열에 따른 값 계산
-        ans = min ( ans, calculate(t1, t2));
-        
-    } while ( next_permutation(vt.begin(), vt.end()));
-    
-    
-    cout << ans ;
-    
-    return 0;
-}
-
 
