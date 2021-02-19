3be2d52cf2db21b6b119a746331114e83036117d
minkyeong <zoz7@naver.com>
2021-02-19 21:22:55 +0900

    [프로그래머스] 크레인 인형뽑기 게임

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,54 +1,45 @@
-#include <iostream>
+#include <string>
+#include <stack>
 #include <algorithm>
-#include <vector>
-#include <utility>
+#include <iostream>
 
 using namespace std;
 
-/*
- [코딜리티] Task 6. NumberOfDiscIntersections - 93% (overflow)
- 
- */
-bool comp(const pair<int, int>& v1, const pair<int, int>& v2){
-    return (v1.first < v2.first);
-}
 
-int solution(vector<int> &A) {
-    int ans = 0;
-    int size = (int)A.size();
-    vector<pair<int, int>> vt; // first : 왼쪽  , second : 오른쪽
+// 크로그래머스 : 크레인 인형뽑기 게임
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    stack<int> st;
     
-    // input
-    for(int i=0; i<size; i++){
-        vt.push_back({i-A[i], i+A[i]});
-    }
-    
-    // 왼쪽을 기준으로 정렬 후, 중복되는 원들을 카운트 시작한다.
-    sort(vt.begin(), vt.end(), comp);
-    //sort(A.begin(), A.end());
-    
-    // i 범위 : size - 1  => 가장 마지막 원은 카운트 대상이 아니기 때문 ( 이미 중복되는 거라면 앞선 idx에서 포함되기 떄문에
-    for(int i=0; i< size-1 ; i++){
-        // i 다음에 있는 원들이 포함되어 있는지 확인한다.
-        for(int j=i+1; j<size; j++){
+    // 움직이는 횟수만큼 탐색한다.
+    for(int i=0; i<moves.size(); i++){
+        
+        int position = moves[i] - 1; // 0 부터 시작해야 하기때문
+        for(int j=0; j< board.size(); j++){
             
-            // 예외처리 1
-            if(ans > 10000000){
-                return -1;
-            }
-
-            // 비교.right < 지금기준원.left 인경우 포함되지 않는 경우로 종료
-            if(vt[i].second < vt[j].first){
+            // 한행씩 아래로 이동해야하기 떄문에 j 로 이동한다.
+            if(board[j][position] != 0){
+                
+                if(!st.empty()  && st.top() == board[j][position]){
+                    st.pop();
+                    answer += 2;
+                }
+                else{
+                    st.push(board[j][position]);
+                }
+                board[j][position] = 0;
                 break;
-            }
-
-            if(vt[i].first <= vt[j].first && vt[i].second >= vt[j].first){
-                ans++;
             }
         }
     }
     
-    return ans;
+    return answer;
 }
 
 
+
+int main(){
+    
+    
+    return 0;
+}
