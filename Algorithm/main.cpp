e3ba496bca18e7e282ad4d0a9349261e260e1227
minkyeong <zoz7@naver.com>
2021-02-19 22:30:12 +0900

    [프로그래머스] 2*n 타일링 - 피보나치 활용

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,45 +1,21 @@
 #include <string>
-#include <stack>
-#include <algorithm>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-// 크로그래머스 : 크레인 인형뽑기 게임
-int solution(vector<vector<int>> board, vector<int> moves) {
+int solution(int n) {
     int answer = 0;
-    stack<int> st;
     
-    // 움직이는 횟수만큼 탐색한다.
-    for(int i=0; i<moves.size(); i++){
-        
-        int position = moves[i] - 1; // 0 부터 시작해야 하기때문
-        for(int j=0; j< board.size(); j++){
-            
-            // 한행씩 아래로 이동해야하기 떄문에 j 로 이동한다.
-            if(board[j][position] != 0){
-                
-                if(!st.empty()  && st.top() == board[j][position]){
-                    st.pop();
-                    answer += 2;
-                }
-                else{
-                    st.push(board[j][position]);
-                }
-                board[j][position] = 0;
-                break;
-            }
-        }
+    int arr[60001] ;
+    
+    // 피보나치의 수 활용
+    arr[1] =1;
+    arr[2] = 2;
+    
+    for(int i=3; i<= n; i++){
+        arr[i] = (arr[i-1] + arr[i-2]) % 1000000007;
     }
     
-    return answer;
-}
-
-
-
-int main(){
     
-    
-    return 0;
+    return arr[n];
 }
