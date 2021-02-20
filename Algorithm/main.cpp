80ce458e9be495cb476a0bf07c707f08d5756301
minkyeong <zoz7@naver.com>
2021-02-20 10:48:36 +0900

    [프로그래머스] 기능개발

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,21 +1,42 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
-int solution(int n) {
-    int answer = 0;
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> speed, process;
     
-    int arr[60001] ;
-    
-    // 피보나치의 수 활용
-    arr[1] =1;
-    arr[2] = 2;
-    
-    for(int i=3; i<= n; i++){
-        arr[i] = (arr[i-1] + arr[i-2]) % 1000000007;
+    // queue 로 옮기기
+    for(int i=0; i<progresses.size(); i++){
+        process.push(progresses[i]);
+        speed.push(speeds[i]);
     }
     
+    int day = 1;
     
-    return arr[n];
+    // 하나씩 큐로 비교하며 효율 100 을 넘었는지 본다.
+    while(!process.empty()){
+        int size= process.size();
+        int finishCnt = 0;
+
+        for(int i=0; i<size; i++){
+            
+            //
+            if((process.front() + (speed.front() * day)) >= 100){
+                finishCnt ++;
+                process.pop();
+                speed.pop();
+            }
+        }
+        
+        if(finishCnt != 0){
+            answer.push_back(finishCnt);
+        }
+        day++;
+    }
+    return answer;
 }
+
