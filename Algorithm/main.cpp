cbb8b9e4bc4bf906a1ff44106800dfb5fca31bb1
minkyeong <zoz7@naver.com>
2021-02-20 12:39:29 +0900

    [프로그래머스] 소수 찾기

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,43 +1,53 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    queue<pair<int, int>> q;
-    priority_queue<int> qOrder; // 순서를 비교하기 위해 선언
+// 소수 판별(에라토스테네스의 체)
+bool isPrime(int num){
     
-    // 큐에 정보 입력
-    for(int i=0; i<priorities.size(); i++){
-        q.push(make_pair(i, priorities[i]));
-        qOrder.push(priorities[i]);
-    }
+    if(num <2) return false;
     
-    
-    while(!q.empty()){
+    for(int i=2; i*i<= num ; i++){
         
-        // 현재 작업 중인 큐의 순번이 우선순위 중 가장 높은지 확인
-        if(q.front().second == qOrder.top()  ){
-            
-            if(q.front().first == location){
-                return answer+1;
-            }
-            else{
-                answer++;
-                q.pop();
-                qOrder.pop();
-            }
+        if(num %i == 0) return false;
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    vector<int> vt;
+    vector<int> num ;
+    
+    // 숫자만 따로 입력 후, 중복 숫자를 제거하기 위해 정렬한다
+    for(int i=0; i<numbers.size(); i++){
+        vt.push_back(numbers[i]);
+    }
+    sort(vt.begin(), vt.end());
+    
+    
+    // 모든 경우의 수
+    do{
+        string tmp = "";
+        for(int i=0; i<vt.size(); i++){
+            tmp.push_back(vt[i]);
+            num.push_back(stoi(tmp)); // 숫자로 형 변환
         }
-        else{
-            // 지금 출력해야 하는 순서가 아니라면, 맨 뒤로 이동
-            q.push(q.front());
-            q.pop();
+        
+    }while (next_permutation(vt.begin(), vt.end()));
+    
+    // 중복 숫자 제거
+    sort(num.begin(), num.end());
+    num.erase(unique(num.begin(),num.end()), num.end());
+    
+    for(int i=0; i<num.size(); i++){
+        // 소수인지 확인 한 후, 값을 증가한다.
+        if(isPrime(num[i])){
+            answer++;
         }
     }
-    
     
     return answer;
 }
