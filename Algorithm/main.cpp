19f426c6e22350a072e139731d5c94066608b8df
minkyeong <zoz7@naver.com>
2021-02-20 11:09:01 +0900

    [프로그래머스] 다리를 지나는 트럭

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,42 +1,50 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-    queue<int> speed, process;
-    
-    // queue 로 옮기기
-    for(int i=0; i<progresses.size(); i++){
-        process.push(progresses[i]);
-        speed.push(speeds[i]);
-    }
-    
-    int day = 1;
-    
-    // 하나씩 큐로 비교하며 효율 100 을 넘었는지 본다.
-    while(!process.empty()){
-        int size= process.size();
-        int finishCnt = 0;
+// 다리 길이는 bridge_length,  다리는 무게 weight
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
 
-        for(int i=0; i<size; i++){
-            
-            //
-            if((process.front() + (speed.front() * day)) >= 100){
-                finishCnt ++;
-                process.pop();
-                speed.pop();
-            }
+    int time = 0;
+    int sumTruct = 0;
+    int idx = 0;
+    queue<int> q;
+    
+    while(1){
+        // 시간은 계속 가는 것을 인지
+        time++;
+        
+        // 트럭이 다리를 지나가면, 전체 다리에 있는 트럭 무게를 뺴준다.
+        if(q.size() == bridge_length){
+            sumTruct -= q.front();
+            q.pop();
         }
         
-        if(finishCnt != 0){
-            answer.push_back(finishCnt);
+        if( truck_weights[idx] +  sumTruct   <= weight ){
+            
+            // 마지막 트럭까지 삽입하고 나면 종료
+            if(idx == truck_weights.size()-1){
+                // 마지막 트럭이 다리를 통과하는 시간 = 다리길이만큼만 더해주면 된다
+                time += bridge_length;
+                break;
+            }
+            
+            q.push(truck_weights[idx]);
+            sumTruct += truck_weights[idx];
+            idx ++;
         }
-        day++;
+        else{
+            // 트럭의 무게가 다리무게를 초과한다면, 트럭을 출발점으로 이동한다
+            q.push(0);
+        }
     }
-    return answer;
+    return time;
 }
 
+int main(){
+    
+    
+    return 0;
+}
