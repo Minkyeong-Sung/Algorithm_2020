12d0cd0f498eb3bbb1da2b8a79184303bfd8ad4a
minkyeong <zoz7@naver.com>
2021-04-04 15:03:19 +0900

    아나그램(구글인터뷰문제)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -4,22 +4,45 @@ using namespace std;
 
 int main(){
     
-    int n, cnt = 0;
-    bool flag = true;
-    cin >> n;
+    string inputA, inputB;
+    cin >>inputA >> inputB;
     
-    for(int i=2; i<=n; i++){
-        flag = true;
-        // 소수는 구하고자 하는 수의제곱근 만큼만 비교해서, 약수가 존재하는지만 판단하면 된다.
-        for(int j=2;j*j<=i; j++){
-            if(i % j==0){
-                flag= false;
-                break;
-            }
+    int arrA[53];
+    
+    memset(arrA, 0, sizeof(arrA));
+    
+    for(int i=0; i<inputA.size(); i++){
+        // 대문자 A(65)-Z(90)
+        if(inputA[i] >= 65 && inputA[i] <= 90){
+            arrA[inputA[i] - 64] ++;
         }
-        if(flag == true) cnt ++;
+        // 소문자 a(97)-z(102)
+        else{
+            arrA[inputA[i] - 70] ++;
+        }
     }
     
-    cout << cnt;
+    // 모든 문자열 구성이 같다면, 해당 배열의 총 합은 0으로 수렴.
+    for(int i=0; i<inputB.size(); i++){
+        // 대문자 A(65)-Z(90)
+        if(inputB[i] >= 65 && inputB[i] <= 90){
+            arrA[inputB[i] - 64] --;
+        }
+        // 소문자 a(97)-z(102)
+        else{
+            arrA[inputB[i] - 70] --;
+        }
+    }
+    
+    for(int i=0; i< 53; i++){
+        if(arrA[i] != 0){
+            cout << "NO\n";
+            exit(0); // 시스템 종료
+        }
+    }
+    
+    cout <<"YES\n";
+    
+    
     return 0;
 }
