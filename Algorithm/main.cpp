e803ef0a53f88fd77f33254c887c9be4d6d5bbb5
minkyeong <zoz7@naver.com>
2021-04-04 14:48:09 +0900

    소수의개수(시간제한 1초)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,60 +1,25 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-// 소수 판별(에라토스테네스의 체)
-bool isPrime(int num){
-    
-    if(num <2) return false;
-    
-    for(int i=2; i*i<= num ; i++){
-        
-        if(num %i == 0) return false;
-    }
-    return true;
-}
-
-int solution(string numbers) {
-    int answer = 0;
-    vector<int> vt;
-    vector<int> num ;
-    
-    // 숫자만 따로 입력 후, 중복 숫자를 제거하기 위해 정렬한다
-    for(int i=0; i<numbers.size(); i++){
-        vt.push_back(numbers[i]);
-    }
-    sort(vt.begin(), vt.end());
-    
-    
-    // 모든 경우의 수
-    do{
-        string tmp = "";
-        for(int i=0; i<vt.size(); i++){
-            tmp.push_back(vt[i]);
-            num.push_back(stoi(tmp)); // 숫자로 형 변환
-        }
-        
-    }while (next_permutation(vt.begin(), vt.end()));
-    
-    // 중복 숫자 제거
-    sort(num.begin(), num.end());
-    num.erase(unique(num.begin(),num.end()), num.end());
-    
-    for(int i=0; i<num.size(); i++){
-        // 소수인지 확인 한 후, 값을 증가한다.
-        if(isPrime(num[i])){
-            answer++;
-        }
-    }
-    
-    return answer;
-}
-
-
 int main(){
     
+    int n, cnt = 0;
+    bool flag = true;
+    cin >> n;
     
+    for(int i=2; i<=n; i++){
+        flag = true;
+        // 소수는 구하고자 하는 수의제곱근 만큼만 비교해서, 약수가 존재하는지만 판단하면 된다.
+        for(int j=2;j*j<=i; j++){
+            if(i % j==0){
+                flag= false;
+                break;
+            }
+        }
+        if(flag == true) cnt ++;
+    }
+    
+    cout << cnt;
     return 0;
 }
