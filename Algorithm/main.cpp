c3d96d2fb89683bbdcc8b491ccd4b9ae7592c818
minkyeong <zoz7@naver.com>
2021-04-13 22:39:43 +0900

    Least Recently Used(카카오 캐시 문제 변형)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,47 +1,48 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(){
     
-    string inputA, inputB;
-    cin >>inputA >> inputB;
+    int s, n , input, pos =0;
+    int i, j;
+    cin >> s >> n;
     
-    int arrA[53];
+    int arr[20];
     
-    memset(arrA, 0, sizeof(arrA));
-    
-    for(int i=0; i<inputA.size(); i++){
-        // 대문자 A(65)-Z(90)
-        if(inputA[i] >= 65 && inputA[i] <= 90){
-            arrA[inputA[i] - 64] ++;
+    for( i=1; i<=n; i++){
+        cin >> input;
+        
+        // 중단점 찾기
+        pos = -1;
+        for( j=0; j<s; j++){
+            if( arr[j] == input){
+                pos = j;
+            }
         }
-        // 소문자 a(97)-z(102)
+        
+        // insert
+        if(pos == -1){
+            for(j = s-1; j>=1; j--){
+                arr[j] = arr[j-1];
+            }
+        }
+        // move and insert
         else{
-            arrA[inputA[i] - 70] ++;
+            for(j = pos; j>=1; j--){
+                arr[j] = arr[j-1];
+            }
         }
+        arr[j] = input;
     }
     
-    // 모든 문자열 구성이 같다면, 해당 배열의 총 합은 0으로 수렴.
-    for(int i=0; i<inputB.size(); i++){
-        // 대문자 A(65)-Z(90)
-        if(inputB[i] >= 65 && inputB[i] <= 90){
-            arrA[inputB[i] - 64] --;
-        }
-        // 소문자 a(97)-z(102)
-        else{
-            arrA[inputB[i] - 70] --;
-        }
+    for(int i=0;i<s; i++){
+        cout << arr[i] << ' ';
     }
     
-    for(int i=0; i< 53; i++){
-        if(arrA[i] != 0){
-            cout << "NO\n";
-            exit(0); // 시스템 종료
-        }
-    }
     
-    cout <<"YES\n";
     
     
     return 0;
