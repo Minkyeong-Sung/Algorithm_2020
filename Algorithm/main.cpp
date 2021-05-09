fd3e73a3850a1ab96c801ad4b449b3aaf12352ca
minkyeong <zoz7@naver.com>
2021-05-09 11:40:47 +0900

    마구간 정하기(이분검색 응용)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -3,33 +3,60 @@
 #include <vector>
 
 using namespace std;
+
 /*
- N개의 수를 오름차순으로 정렬한 다음 N개의 수 중 한 개의 수인 M이 주어지면 이분검색으로 M이 정렬된 상태에서 몇 번째에 있는지
+ 
+ N개의 마구간이 1차원 수직선상에 있습니다. 각 마구간은 x1, x2, x3, ......, xN의 좌표를 가 지며, 마구간간에 좌표가 중복되는 일은 없습니다.
+ 현수는 C마리의 말을 가지고 있는데, 이 말들은 서로 가까이 있는 것을 좋아하지 않습니다. 각 마구간에는 한 마리의 말만 넣을 수 있고, 가장 가까운 두 말의 거리가 최대가 되게 말을 마구간에 배치하고 싶습니다.
+ C마리의 말을 N개의 마구간에 배치했을 때 가장 가까운 두 말의 거리가 최대가 되는 그 최대 값을 출력하는 프로그램을 작성하세요.
  */
+
+int n;
+vector<int> vt;
+
+int isPossible(int num){
+
+    int cnt =1;
+    int position = vt[0];
+    
+    // 두번쨰 좌표부터 검증
+    for(int i=1; i<n; i++){
+        
+        if(vt[i] - position >= num ){
+            cnt++;
+            position = vt[i];
+        }
+    }
+    
+    return cnt;
+}
+
 
 int main(){
     
-    int n, m;
-    cin >> n >> m;
-    vector<int> vt(n);
+    int  c;
+    int num;
     
+    cin >> n >> c;
+
     for(int i=0; i<n; i++){
-        cin >> vt[i];
+        cin >> num;
+        vt.push_back(num);
     }
     
-    int lt=0, rt = n-1;
-    int mid = 0;
     sort(vt.begin(), vt.end());
     
-    while(lt <= rt){
+    int lt = 0, rt = vt.size() -1;
+    int mid  = 0;
+    int max = 0;
+    int result = 0;
+    
+    while(lt<= rt){
         
         mid = (lt + rt)/2;
         
-        if(vt[mid] == m){
-            cout << mid + 1;
-            break;
-            
-        }else if(vt[mid] < m){
+        if(isPossible(mid) >= c){
+            result =  mid;
             lt = mid + 1;
         }
         else{
@@ -37,6 +64,8 @@ int main(){
         }
     }
     
+    cout << result;
     
     return 0;
+    
 }
