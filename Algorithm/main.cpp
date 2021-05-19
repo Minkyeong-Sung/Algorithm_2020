082322c67d28a3f477e3eb988a9a679ab23273f3
minkyeong <zoz7@naver.com>
2021-05-19 12:37:49 +0900

    64. 경로 탐색(DFS)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -3,62 +3,47 @@
 
 using namespace std;
 
-int arr[101];
-int data[101];
+int arr[21][21];
+int visit[21]; //
+int n, cnt = 0;
 
-void divide(int l, int r){
-    int p1, p2, p3;
-    if(l<r){
-        // divide
-        int mid = (l + r)/2;
-        
-        divide(l, mid);
-        divide(mid + 1, r);
-        
-        // merge
-        // 비교한 값을 담기 위해 선언한 포인터
-        p1 = l;
-        p2 = mid+1;
-        p3 = l;
-        
-        while(p1 <= mid &&  p2 <= r){
-            if(arr[p1] < arr[p2]){
-                data[p3++] = arr[p1++];
-            }
-            else{
-                data[p3++] = arr[p2++];
+void DFS(int num){
+    
+    if(num == n){
+        cnt ++;
+    }
+    else{
+        for(int i=1; i<= n; i++){
+            if(arr[num][i] == 1 && visit[i] == 0){
+                // 방문 표시
+                visit[i] = 1;
+                DFS(i);
+                // 다시 방문 해제
+                visit[i] = 0;
+                
             }
         }
-        // 왼쪽 나머지
-        while(p1 <= mid){
-            data[p3++] = arr[p1++];
-        }
-        // 오른쪽 나머지
-        while(p2<= r){
-            data[p3++] = arr[p2++];
-        }
+         
         
-        // 재 정렬 완료 된 배열
-        for(int i = l ; i<= r; i++){
-            arr[i] = data[i];
-        }
     }
 }
 
 int main(){
+    int m, x, y;
+    cin >> n >> m;
     
-    int n;
-    cin >> n;
+    memset(arr, 0, sizeof(arr));
     
-    for(int i=1; i<=n; i++){
-        cin >> arr[i];
+    for(int i=1; i<=m; i++){
+        cin >> x >> y;
+        arr[x][y] = 1;
     }
     
-    divide(1, n);
+    visit[1] = 1;
+          
+    DFS(1);
     
-    for(int i=1; i<=n; i++){
-        cout << arr[i];
-    }
+    cout << cnt;
     
     return 0;
 }
