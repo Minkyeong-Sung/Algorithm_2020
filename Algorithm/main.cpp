229a4de56382fca45d1e731bb87f2220d393be87
minkyeong <zoz7@naver.com>
2021-05-19 11:45:57 +0900

    62. 병합정렬(basic)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -3,36 +3,62 @@
 
 using namespace std;
 
-int n;
-int ch[11];
+int arr[101];
+int data[101];
 
-void DFS(int num){
-    
-    if(num == n+1){
-        for(int i=1; i<=n;i++){
-            if(ch[i] == 1){
-                cout << i  << ' ' ;
+void divide(int l, int r){
+    int p1, p2, p3;
+    if(l<r){
+        // divide
+        int mid = (l + r)/2;
+        
+        divide(l, mid);
+        divide(mid + 1, r);
+        
+        // merge
+        // 비교한 값을 담기 위해 선언한 포인터
+        p1 = l;
+        p2 = mid+1;
+        p3 = l;
+        
+        while(p1 <= mid &&  p2 <= r){
+            if(arr[p1] < arr[p2]){
+                data[p3++] = arr[p1++];
+            }
+            else{
+                data[p3++] = arr[p2++];
             }
         }
-        cout << '\n';
-    }
-    else{
-        // left : 사용 o
-        ch[num] = 1;
-        DFS(num+1);
+        // 왼쪽 나머지
+        while(p1 <= mid){
+            data[p3++] = arr[p1++];
+        }
+        // 오른쪽 나머지
+        while(p2<= r){
+            data[p3++] = arr[p2++];
+        }
         
-        // right : 사용 x
-        ch[num] = 0;
-        DFS(num+1);
+        // 재 정렬 완료 된 배열
+        for(int i = l ; i<= r; i++){
+            arr[i] = data[i];
+        }
     }
-    
 }
 
 int main(){
     
+    int n;
     cin >> n;
     
-    DFS(1);
+    for(int i=1; i<=n; i++){
+        cin >> arr[i];
+    }
+    
+    divide(1, n);
+    
+    for(int i=1; i<=n; i++){
+        cout << arr[i];
+    }
     
     return 0;
 }
