98d51b91d911295c2d054d77e624303e383be31a
minkyeong <zoz7@naver.com>
2021-05-22 18:32:24 +0900

    72. 공주 구하기(큐 자료구조로 해결)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -7,38 +7,34 @@ using namespace std;
 int main(){
     
     int n, m;
-    int visit[100001];
     queue<int> q;
     
     cin >> n >> m;
     
-    memset(visit, 0, sizeof(100001));
+    for(int i=1; i<= n; i++){
+        q.push(i);
+    }
     
-    q.push(n);
-    visit[n] = 1;
-    
-    // +1, -1, +5 만큼 이동할 배열
-    int dir[3] = {1, -1, 5};
-    
+    int cnt = 0;
     while(!q.empty()){
+        
+        if(q.size() == 1){
+            break;
+        }
+        
+        cnt ++;
         int next = q.front();
         q.pop();
         
-        for(int d=0; d<3; d++){
-            
-            if(next + dir[d] == m){
-                cout << visit[next] ;
-                return 0;
-            }
-            
-            if(visit[next + dir[d]] == 0){
-                visit[next + dir[d]] = visit[next] + 1;
-                q.push(next + dir[d]);
-            }
+        if(cnt == m){
+            cnt = 0;
+        }
+        else{
+            q.push(next);
         }
     }
     
-    
+    cout << q.front();
     
     return 0;
 }
