c1b16d5d7edd62a64b99e510bbbed27f6748b401
minkyeong <zoz7@naver.com>
2021-05-22 17:53:18 +0900

    71. 송아지 찾기(BFS : 상태트리탐색)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,55 +1,43 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
 int main(){
     
-    int n, m, v, u;
+    int n, m;
+    int visit[100001];
+    queue<int> q;
     
     cin >> n >> m;
     
-    vector<int> vt[21];
-    queue<int> q;
-    int visit[21];
-    int dist[21];
+    memset(visit, 0, sizeof(100001));
     
-    for(int i=0; i<m; i++){
-        cin >> v >> u;
-        vt[v].push_back(u);
-        
-        // init
-        dist[i] = 0;
-        visit[i] = 0;
-    }
+    q.push(n);
+    visit[n] = 1;
     
-    q.push(1);
-    visit[1] = 1;
+    // +1, -1, +5 만큼 이동할 배열
+    int dir[3] = {1, -1, 5};
     
     while(!q.empty()){
-        
         int next = q.front();
         q.pop();
         
-        for(int i=0; i< vt[next].size(); i++){
+        for(int d=0; d<3; d++){
             
-            if(visit[vt[next][i]] == 0){
-                
-                visit[vt[next][i]] = 1;
-                q.push(vt[next][i]);
-                
-                // 다음 좌표까지 가는 최소 간선의 수
-                // 현재까지 온 간선 수 + 1 이 최소 간선의 길이 
-                dist[vt[next][i]] = dist[next] + 1;
+            if(next + dir[d] == m){
+                cout << visit[next] ;
+                return 0;
+            }
+            
+            if(visit[next + dir[d]] == 0){
+                visit[next + dir[d]] = visit[next] + 1;
+                q.push(next + dir[d]);
             }
         }
     }
     
-    for(int i=2; i<= n; i++){
-        cout << i << " : " << dist[i] << '\n';
-    }
     
     
     return 0;
