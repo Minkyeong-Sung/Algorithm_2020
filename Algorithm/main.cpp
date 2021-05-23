8a6285a85b97d7a249ff87cc23c3913d1836db3f
minkyeong <zoz7@naver.com>
2021-05-23 14:00:19 +0900

    77. 친구인가? (Union&Find 자료구조)

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -6,29 +6,52 @@
 
 using namespace std;
 
-// 메모이제이션 사용
-// 해당 배열의 값을 미리 저장해두고 사용
-int dy[21][21];
+int unf[10001];
 
-int DFS(int n, int r){
-    
-    // 미리 구한 값을 바로 사용 : 시간절약
-    if(dy[n][r] > 0) return dy[n][r];
-    
-    if( n == r || r == 0){
-        return 1;
+//메모이제이션 활용 & 해당 배열 찾기
+int Find(int n){
+    if(unf[n] == n){
+        return  n;
     }
     else{
-        // n-1 개중 r-1 만큼만 뽑겠다 , n-1개중 r 만큼 모두(내가 참여하지 않는 경우) 뽑겠다
-        return  dy[n][r]  = DFS(n-1, r-1) + DFS(n-1, r);
+        return unf[n] = Find(unf[n]);
+    }
+}
+// 찾은 후, 합치기 
+void Union(int a, int b){
+    
+    a = Find(a);
+    b = Find(b);
+    
+    // 합치기
+    if(a != b){
+        unf[a] = b;
     }
 }
 
 int main(){
     int n, m;
+    int a, b;
     cin >> n >> m;
-    cout << DFS(n, m);
+    for(int i=1; i<= n; i++){
+        unf[i] = i;
+    }
+    for(int i=0; i<m; i++){
+        cin >> a >> b;
+        Union(a, b);
+    }
+    int s , e;
+    cin >> s >> e;
     
+    a = Find(s);
+    b = Find(e);
+    
+    if(a == b){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
     
     return 0;
 }
